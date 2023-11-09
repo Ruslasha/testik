d6e34465deab180c43aa7bc7c980d5a6433df00b
Ruslan <ruslan99992222@gmail.com>
2023-11-09 09:19:39 +0300

    Initial Commit

---
+++ testik/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testik
+//
+//  Created by Руслан Абрамов on 09.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
